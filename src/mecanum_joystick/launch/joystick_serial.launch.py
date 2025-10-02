184fc350c9519bdeca517172ff438ddcd535ee6d
adam112113 <115705424+adam112113@users.noreply.github.com>
2025-10-02 12:21:52 +0200

    Update joystick_serial.launch.py
    Changing scale factors

--- src/mecanum_joystick/launch/joystick_serial.launch.py
+++ src/mecanum_joystick/launch/joystick_serial.launch.py
@@ -26,9 +26,9 @@ def generate_launch_description():
                 "axis_linear.x": 1,
                 "axis_linear.y": 0,
                 "axis_angular.yaw": 3,
-                "scale_linear.x": 0.5,
-                "scale_linear.y": 0.5,
-                "scale_angular.yaw": 0.5,
+                "scale_linear.x": 0.1,
+                "scale_linear.y": 0.1,
+                "scale_angular.yaw": 0.1,
                 # set buttons/enable as you like
                 "enable_button": 6,
                 "enable_turbo_button": 7,
@@ -45,7 +45,7 @@ def generate_launch_description():
             parameters=[{
                 "port": "/dev/ttyACM0",
                 "baudrate": 115200,
-                "wheel_radius": 0.033,
+                "wheel_radius": 0.03,
                 "wheel_separation_x": 0.375,
                 "wheel_separation_y": 0.290,
                 "max_rpm": 10,
